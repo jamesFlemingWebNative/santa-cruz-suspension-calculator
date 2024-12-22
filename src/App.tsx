5aded941a454f67a40e7433427e4d6a94fddc866
James Fleming <james@webnative.com.au>
2024-12-23 08:24:53 +1000

    Initial commit - Project setup with Vite, Tailwind CSS, and Headless UI

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <img src="../assets/santa-cruz-logo.png" alt="Santa Cruz Logo" />
+      </div>
+    </>
+  );
+}
+
+export default App;
